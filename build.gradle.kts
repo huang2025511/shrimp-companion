4d77ab90afa4591950d262018fbe0afbdce3853d
huang20260511 <huang20260511@gitee.com>
2026-06-02 04:30:36 +0000

    Add Gradle wrapper and update build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,21 @@
-plugins {
-    id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
